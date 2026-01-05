f6bb9ee53990ee8816ac36ce41a8fab7cbdc55a0
jinseok kim <121952875+jinseok19@users.noreply.github.com>
2026-01-05 17:54:16 +0900

    Add: homepage back button

--- components/layout.js
+++ components/layout.js
@@ -96,10 +96,19 @@ export function mountHeader(el, { activePath } = {}) {
   el.innerHTML = `
     <div class="header">
       <div class="header-content">
-        <a class="logo" href="/">
-          <span class="logo-icon">${brand.logo}</span>
-          <span class="logo-text">${brand.name}</span>
-        </a>
+        <div class="brand-container">
+          <a class="nexuscore-home-link" href="https://nexuscore.all4fit.co.kr" title="NexusCore 홈으로">
+            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+              <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+              <polyline points="9 22 9 12 15 12 15 22"></polyline>
+            </svg>
+            <span>NexusCore</span>
+          </a>
+          <a class="logo" href="/">
+            <span class="logo-icon">${brand.logo}</span>
+            <span class="logo-text">${brand.name}</span>
+          </a>
+        </div>
         
         <nav class="primary-nav" aria-label="Primary navigation">
           <ul class="nav-list">
