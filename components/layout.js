59d70ffc2cea9e29fe83dfc0970fb2ad24ae96a2
jinseok kim <121952875+jinseok19@users.noreply.github.com>
2026-01-15 17:14:21 +0900

    Update NexusCore link to nexuscore.co.kr

--- components/layout.js
+++ components/layout.js
@@ -97,7 +97,7 @@ export function mountHeader(el, { activePath } = {}) {
     <div class="header">
       <div class="header-content">
         <div class="brand-container">
-          <a class="nexuscore-home-link" href="https://nexuscore.all4fit.co.kr" title="NexusCore 홈으로">
+          <a class="nexuscore-home-link" href="https://nexuscore.co.kr" title="NexusCore 홈으로">
             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
               <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
               <polyline points="9 22 9 12 15 12 15 22"></polyline>
